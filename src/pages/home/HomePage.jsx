470a5287adb7ff0ba80adcc05b4de1be83ca48cf
ishu shu <ishushu0307@gmail.com>
2025-08-18 15:23:16 +0530

    homepage style completed

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
 import "./Homepage.scss";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import banner from "../../assets/banner.png";
+import HowItWorks from "../../components/howitsworks/Howitsworks"
+import bannerimge from "../../assets/banner.png";
+import pettransport from "../../assets/easy-transport.png";
+import original from "../../assets/original.png";
+import certified from "../../assets/certified.png";
+import secure from "../../assets/secure.png";
+import click from "../../assets/click.png";
+import petsGroup from "../../assets/friends.png";
+import yellowSparkle from "../../assets/yellowSparkle.png";
+import yellowShapes from "../../assets/frdsroundshape.png";
 function Homepage() {
     const [index, setIndex] = useState(0);
     const prevSlide = () => {
@@ -12,7 +19,6 @@ function Homepage() {
         if (index < pets.length - 3) setIndex(index + 1);
     };
     const [pets, setPets] = useState([]);
-    console.log(pets, "dasdasdsa")
     useEffect(() => {
         fetch("http://localhost:3000/data/petlist.json")
             .then((res) => res.json())
@@ -21,20 +27,24 @@ function Homepage() {
 
     return (
         <>
-            <div>
-                <div className="top-banner">
-                    <div className="banner">
-                        <div className="bannertext">
+            <div className="homepage">
+                <section className="top-banner">
+                    <div className="banner-inner">
+                        <div className="banner-text">
                             <h2> “Find your forever friend”</h2>
                             <p>"Browse hundreds of lovable pets waiting for their forever homes." "Browse hundreds of lovable pets waiting for their forever homes.""Browse hundreds of lovable pets waiting for their forever homes."</p>
-                            <a href="browsepets" className="browsebtn"> Browse Pets</a>
-                            <a href="#view-pet" className="listbtn"> List Your Pet </a>
+                            <div className="btn">
+                                <a href="browsepets" className="browsebtn"> Browse Pets</a>
+                                <a href="#view-pet" className="listbtn"> List Your Pet </a>
+                            </div>
                         </div>
                         <div>
-                            <img src={banner} />
+                            <img src={bannerimge} alt="banner" className="banner-img" />
                         </div>
                     </div>
-                </div>
+                </section>
+                <HowItWorks />
+                
                 <section>
                     <div className="pet-carousel">
                         <h2 className="title" id="view-pet">List Your Pet</h2>
@@ -46,7 +56,6 @@ function Homepage() {
 
                             <div className="cards-container">
                                 {pets.slice(index, index + 3).map((pet) => (
-                                    console.log(pet, "petdata"),
                                     <div className="pet-card" key={pet.id}>
                                         <img src={pet.image} alt={pet.name} className="pet-image" />
                                         <div className="card-body">
@@ -65,7 +74,53 @@ function Homepage() {
                         </div>
                     </div>
                 </section>
+                <section>
+                    <div className="whychoose">
+                        <h2>Why Choose Pet Match?</h2>
+                        <div className="petchoose">
+                            <div>
+                                <img src={pettransport} alt="Easy & Transparent Booking" />
+                            </div>
+                            <div>
+                                <img src={original} alt="No spam, no scams" />
+                            </div>
+                            <div>
+                                <img src={certified} alt="Support Local Shelters" />
+                            </div>
+                            <div>
+                                <img src={secure} alt="Verified Listings Only" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section className="ourservices">
+                    <h2> Our Services:</h2>
+                    <div className="services">
+                        <div className="one">
+                            <p><img src={click} alt="Tickmark green" />List Your Pet for Adoption</p>
+                            <p><img src={click} alt="Tickmark green" />Location-Based Pet Search</p>
+                            <p><img src={click} alt="Tickmark green" />Adoption Follow-Up Assistance</p>
+                        </div>
+                        <div className="two">
+                            <p><img src={click} alt="Tickmark green" />Vaccination & Health Info</p>
+                            <p><img src={click} alt="Tickmark green" />Visit Scheduling & Payments</p>
+                            <p><img src={click} alt="Tickmark green" />Pet Adoption Support</p>
+                        </div>
+                    </div>
 
+                </section>
+                <section className="pets-banner">
+                    <img src={yellowShapes} alt="round" className="decor-shapes" />
+                    <img src={yellowSparkle} alt="star" className="decor-sparkle" />
+                    <img src={petsGroup} alt="Pets" className="pets-image" />
+
+                    <div className="banner-content">
+                        <h2>"Find your forever friend"</h2>
+                        <p>Browse hundreds of lovable pets waiting for their forever homes.</p>
+                        <a href="browsepets" className="browsebtn">Browse Pets</a>
+                    </div>
+
+                </section>
             </div>
 
 
